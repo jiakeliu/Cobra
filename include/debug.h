3b3863b5bc1b533e409a4f683787de7513fc6eec
ttm0011 <ttm0011@7a5ebaa7-63b7-0410-984a-e5fb08a8dfaf>
2012-01-26 02:57:19 +0000

    pre-everything.
    git-svn-id: svn+ssh://zeus.eng.uah.edu/home/work/cpe453/team8/cobra/trunk@2 7a5ebaa7-63b7-0410-984a-e5fb08a8dfaf

---
+++ include/debug.h
@@ -0,0 +1,8 @@
+#ifndef __DEBUG_H__
+#define __DEBUG_H__
+
+class logger {
+	public:
+};
+
+#endif /* __DEBUG_H__ */
---
+++ src/main.cc
@@ -0,0 +1,8 @@
+#include <QApplication>
+
+int
+main(int argc, char* argv[]) {
+	return 0;
+}
+
+// vim: noai:ts=4:sw=4
